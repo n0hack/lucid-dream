da8d48cf615c9fb38389525d39782b5a7e9ce130
Lucid <nohack-@naver.com>
2023-11-10 09:50:08 +0900

    chore: prettier 설정

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-astro', '@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  importOrder: ['^react', '<THIRD_PARTY_MODULES>', '^@', '^[./]'],
+};
+
+export default config;
