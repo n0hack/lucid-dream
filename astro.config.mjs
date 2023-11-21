b92c1163cc326aec5e9aecd7237cd5a6e73cd387
Lucid <nohack-@naver.com>
2023-11-21 21:52:44 +0900

    chore: tailwind 마이그레이션

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import tailwindcss from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
-import pandacss from '@pandacss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), pandacss()],
+  integrations: [react(), tailwindcss(), mdx()],
   output: 'hybrid',
   adapter: vercel(),
 });
