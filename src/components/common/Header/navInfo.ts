d8bec184264b30135ae2518af4fc30597d8c145a
Lucid <nohack-@naver.com>
2023-11-24 19:55:34 +0900

    fix: 리다이렉트 안 되는 문제 우회

--- src/components/common/Header/navInfo.ts
+++ src/components/common/Header/navInfo.ts
@@ -6,12 +6,12 @@ const navInfo = [
   },
   {
     name: '프로젝트',
-    path: '/project',
+    path: '/project/page/1',
     animation: 'animate-[showMenuItem_0.75s_forwards_0.2s]',
   },
   {
     name: '스토리',
-    path: '/story',
+    path: '/story/all/page/1',
     animation: 'animate-[showMenuItem_0.75s_forwards_0.4s]',
   },
 ];
