d3f952bc66ad0c5c77ded4e5a659414b76e1bef7
Lucid <nohack-@naver.com>
2023-11-24 19:23:51 +0900

    fix: 빌드 문제 해결 (Hydration)

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,3 +1,5 @@
 export { default as HomeMainImage } from './HomeMainImage.astro';
-export { default as HomeProjectSection } from './HomeProjectSection';
-export { default as HomeStorySection } from './HomeStorySection';
+export { default as HomeProjectSection } from './HomeProjectSection.astro';
+export { default as HomeProjectSlider } from './HomeProjectSlider';
+export { default as HomeStorySection } from './HomeStorySection.astro';
+export { default as HomeStorySlider } from './HomeStorySlider';
