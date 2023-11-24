5217ef82d85d5b3d4cf4bf354cd1f4e67bbaf1d1
Lucid <nohack-@naver.com>
2023-11-24 19:12:04 +0900

    feat: 소개 페이지

--- src/components/about/AboutMarkdownRenderer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Responsive } from '@components/common';
-import styles from './AboutMarkdownRenderer.module.scss';
-
-const AboutMarkdownRenderer = ({ children }: React.PropsWithChildren) => {
-  return <Responsive className={styles.renderer}>{children}</Responsive>;
-};
-
-export default AboutMarkdownRenderer;
--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -1,2 +1,3 @@
-export { default as AboutMe } from './AboutMe';
-export { default as AboutMarkdownRenderer } from './AboutMarkdownRenderer';
+export { default as AboutMarkdownRenderer } from './AboutMarkdownRenderer.astro';
+export { default as AboutMarkdownSection } from './AboutMarkdownSection.astro';
+export { default as AboutMe } from './AboutMe.astro';
