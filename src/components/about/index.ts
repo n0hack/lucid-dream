c96a41eb4c99eacd71d4fec027f4965f19ffe343
Lucid <nohack-@naver.com>
2023-11-27 20:27:51 +0900

    refactor: About 페이지 리팩토링

--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -1,3 +1,2 @@
 export { default as AboutMarkdownRenderer } from './AboutMarkdownRenderer.astro';
-export { default as AboutMarkdownSection } from './AboutMarkdownSection.astro';
 export { default as AboutMe } from './AboutMe.astro';
