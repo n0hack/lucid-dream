61704165c118065d939ffda2846b4b1c0a7a8287
Lucid <nohack-@naver.com>
2023-11-29 04:47:16 +0900

    chore: sitemap & robot 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import expressiveCode from 'astro-expressive-code';
 import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
@@ -14,10 +15,16 @@ const expressiveCodeOptions = {
   defaultLocale: 'ko-KR',
 };
 
+/** @type {import("@astrojs/sitemap").SitemapOptions} */
+const sitemapOptions = {
+  filter: (page) => !page.includes('https://lucid-dream.net/search'),
+};
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwindcss(), expressiveCode(expressiveCodeOptions), mdx()],
+  integrations: [react(), tailwindcss(), expressiveCode(expressiveCodeOptions), mdx(), sitemap(sitemapOptions)],
   output: 'hybrid',
+  site: 'https://lucid-dream.net',
   adapter: vercel(),
   image: {
     service: passthroughImageService(),
