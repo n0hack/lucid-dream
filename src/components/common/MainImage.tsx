2d4a49447dfa81274854d8eba4a300d9bff604e2
Lucid <nohack-@naver.com>
2023-11-28 22:47:33 +0900

    chore: 이미지 최적화 (90% 손실 압축)

--- src/components/common/MainImage.tsx
+++ src/components/common/MainImage.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
 
-type BackgroundImageProps = React.ComponentPropsWithoutRef<'img'>;
+type MainImageProps = React.ComponentPropsWithoutRef<'img'>;
 
-const BackgroundImage = ({ className, ...rest }: BackgroundImageProps) => {
+const MainImage = ({ className, ...rest }: MainImageProps) => {
   return (
     <img
       className={twMerge('h-full w-full object-cover', className)}
-      src="/images/main@3140.webp"
+      src="/images/main@1280.webp"
       srcSet="/images/main@1280.webp 1280w, /images/main@1920.webp 1920w, /images/main@3140.webp 3140w"
       alt="배경"
       {...rest}
@@ -15,4 +15,4 @@ const BackgroundImage = ({ className, ...rest }: BackgroundImageProps) => {
   );
 };
 
-export default BackgroundImage;
+export default MainImage;
