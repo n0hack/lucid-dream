40bea0c0de49cb962734055b56419ecb66122376
Lucid <nohack-@naver.com>
2023-11-28 14:52:06 +0900

    feat: 폰트 preload 적용

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,9 +7,6 @@ export const HEADER_TRANSITION_POINT = 64;
 /** 스크롤 동작 제어를 담당하는 DOM 클래스 */
 export const CLASSNAME_PREVENT_SCROLL = 'prevent-scroll';
 
-/** 메인으로 사용하는 이미지 경로 */
-export const MAIN_IMAGE_SRC = (width: '1280' | '1920' | '3140' = '3140') => `/images/main@${width}.webp`;
-
 /** 인덱스 페이지 슬라이드에 노출할 프로젝트 최대 개수 */
 export const PROJECT_SLIDE_MAX_COUNT = 10;
 
@@ -19,11 +16,11 @@ export const STORY_SLIDE_MAX_COUNT = 6;
 /** 검색 결과에 노출할 스토리 최대 개수 (무한 스크롤) */
 export const SEARCH_RESULT_MAX_COUNT = 9;
 
-/** SEO */
-export const SEO = {
-  SITE_NAME: '루시드 드림 𖤐',
-  SITE_URL: 'https://lucid-dream.net',
-  AUTHOR: 'Lucid Jeon',
-  IMAGE: MAIN_IMAGE_SRC('1280'),
-  KEYWORDS: ['루시드 드림', 'lucid dream', '화려히 반짝일 나의 스토리'],
-};
+// /** SEO */
+// export const SEO = {
+//   SITE_NAME: '루시드 드림 𖤐',
+//   SITE_URL: 'https://lucid-dream.net',
+//   AUTHOR: 'Lucid Jeon',
+//   IMAGE: MAIN_IMAGE_SRC('1280'),
+//   KEYWORDS: ['루시드 드림', 'lucid dream', '화려히 반짝일 나의 스토리'],
+// };
