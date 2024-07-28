f35c93fd69f57a45520360a716e926989453fe8d
Lucid <nohack-@naver.com>
2024-07-28 21:55:30 +0900

    [chore] 불필요한 내용 제거

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,7 +1,6 @@
 export const SITE_NAME: string = '루시드 드림';
 
-export const SITE_DESCRIPTION: string =
-  '다양한 상상을 현실로 만드는 멀티 크리에이터를 꿈꾸는 루시드의 개발 블로그 ˶•⩊•˶';
+export const SITE_DESCRIPTION: string = '다양한 상상을 현실로 만드는 멀티 크리에이터를 꿈꾸는 루시드의 개발 블로그';
 
 export const SITE_URL: string = 'https://www.lucid-dream.net';
 
